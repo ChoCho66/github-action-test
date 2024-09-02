848b31547ceb48b5eb79b0ad33b442d7a9d8704a
ChoCho66 <porkdie@gmail.com>
2024-09-02 14:55:28 +0800

    update response.status_code == 200

--- scripts/scrape-ani-gamer.py
+++ scripts/scrape-ani-gamer.py
@@ -1,28 +1,32 @@
 import requests_html
-import json
+# import json
 
 url = "https://ani.gamer.com.tw/"
 session = requests_html.HTMLSession()
 response = session.get(url)
 
-# 初始化一个列表来存储数据
-data_list = []
-# 爬取数据并存储到列表中
-for i in range(6):
-    name = response.html.find('.anime-name-block')[i].find('p')[1].text
-    description = response.html.find('.anime-name-block')[i].find('p')[2].text
-    
-    # 将每一项数据存储到字典中
-    data_dict = {
-        'name': name,
-        'description': description
-    }
-    
-    # 将字典添加到列表中
-    data_list.append(data_dict)
-    
-print(data_list)
+# 检查请求是否成功
+if response.status_code == 200:
+    # 初始化一个列表来存储数据
+    data_list = []
+    # 爬取数据并存储到列表中
+    for i in range(6):
+        name = response.html.find('.anime-name-block')[i].find('p')[1].text
+        description = response.html.find('.anime-name-block')[i].find('p')[2].text
+        
+        # 将每一项数据存储到字典中
+        data_dict = {
+            'name': name,
+            'description': description
+        }
+        
+        # 将字典添加到列表中
+        data_list.append(data_dict)
+        
+    print(data_list)
 
-# # 将列表数据写入 JSON 文件
-# with open('anime_data.json', 'w') as f:
-#     json.dump(data_list, f, ensure_ascii=False, indent=4)
+    # # 将列表数据写入 JSON 文件
+    # with open('anime_data.json', 'w') as f:
+    #     json.dump(data_list, f, ensure_ascii=False, indent=4)
+else:
+    print(f"请求失败，状态码：{response.status_code}")
