9a26629e28633e9e745d1c15e22ecbf795151597
ChoCho66 <porkdie@gmail.com>
2024-09-04 22:20:00 +0800

    print all href

--- scripts/scrape-selenium-ani.py
+++ scripts/scrape-selenium-ani.py
@@ -11,7 +11,7 @@ firefox_options.add_argument("--headless")  # 在無頭模式下運行
 # 設定 FirefoxDriver
 driver = webdriver.Firefox(service=Service(), options=firefox_options)
 
-url = "https://www.twitch.tv/"
+url = "https://ani.gamer.com.tw"
 
 try:
     # 瀏覽目標網站
@@ -20,15 +20,26 @@ try:
     # print content of the page
     # print(driver.page_source)
 
-    __ = driver.find_elements(By.CSS_SELECTOR, ".Layout-sc-1xcs6mc-0.jxGBqp")[0]
-    # print(__)
+    # 找到所有 class = "anime-name-block" 的元素
+    anime_blocks = driver.find_elements(By.CLASS_NAME, "anime-name-block")
+    print(anime_blocks[:10])
     
-    # print all href in __
-    elements = __.find_elements(By.TAG_NAME, "a")  # 找到所有的 <a> 標籤
+    print()
+    print()
+    print("-" * 40)  # 分隔線
 
-    hrefs = [element.get_attribute("href") for element in elements]  # 獲取所有的 href
-    print(hrefs)  # 輸出所有的 href
+    # 迭代前10個 anime-name-block 元素
+    for index, block in enumerate(anime_blocks[:10]):
+        print(f"Anime Block {index + 1}:")
 
+        # 找到該 anime-name-block 中所有 p 元素
+        p_elements = block.find_elements(By.TAG_NAME, "p")
+
+        # 打印每個 p 元素的文本
+        for p in p_elements:
+            print(p.text)
+
+        print("-" * 40)  # 分隔線
 
 except Exception as e:
     # 捕獲並打印錯誤訊息
